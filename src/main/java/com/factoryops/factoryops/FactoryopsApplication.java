1b969a8bbc38dbf5cc541ca4cf44f620b54bdbb3
Niels-Patrick <73878385+Niels-Patrick@users.noreply.github.com>
2026-08-03 16:48:59 +0200

    Spring Boot project initialization

---
+++ src/main/java/com/factoryops/factoryops/FactoryopsApplication.java
@@ -0,0 +1,13 @@
+package com.factoryops.factoryops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryopsApplication.class, args);
+	}
+
+}
