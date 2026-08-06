aeb7ce98daff0f4830b2794e8920034eeed85b68
Niels-Patrick <73878385+Niels-Patrick@users.noreply.github.com>
2026-08-06 17:51:12 +0200

    Update MachineService.java
    Building the first business method for Machine.

--- src/main/java/com/factoryops/factoryops/service/MachineService.java
+++ src/main/java/com/factoryops/factoryops/service/MachineService.java
@@ -3,6 +3,9 @@ package com.factoryops.factoryops.service;
 import org.springframework.stereotype.Service;
 
 import com.factoryops.factoryops.repository.MachineRepository;
+import com.factoryops.factoryops.dto.CreateMachineRequest;
+import com.factoryops.factoryops.dto.MachineResponse;
+import com.factoryops.factoryops.entity.Machine;
 
 @Service
 public class MachineService {
@@ -10,5 +13,33 @@ public class MachineService {
 	
 	public MachineService(MachineRepository machineRepository) {
 		this.machineRepository = machineRepository;
+	}
+	
+	
+	public MachineResponse createMachine(CreateMachineRequest request) {
+		Machine machine = new Machine();
+		
+		machine.setName(request.getName());
+		machine.setSerialNumber(request.getSerialNumber());
+		machine.setManufacturer(request.getManufacturer());
+		machine.setProductionLine(request.getProductionLine());
+		machine.setInstallationDate(request.getInstallationDate());
+		machine.setStatus(request.getStatus());
+		machine.setOperatingHours(request.getOperatingHours());
+		
+		Machine savedMachine = machineRepository.save(machine);
+		
+		MachineResponse response = new MachineResponse();
+		
+		response.setId(savedMachine.getId());
+		response.setName(savedMachine.getName());
+		response.setSerialNumber(savedMachine.getSerialNumber());
+		response.setManufacturer(savedMachine.getManufacturer());
+		response.setProductionLine(savedMachine.getProductionLine());
+		response.setInstallationDate(savedMachine.getInstallationDate());
+		response.setStatus(savedMachine.getStatus());
+		response.setOperatingHours(savedMachine.getOperatingHours());
+		
+		return response;
 	}
 }
